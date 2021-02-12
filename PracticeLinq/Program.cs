830de4cd79984c6e37b3d5c0cfbc7f9fe3b06ffc
abitj <https://github.com/abitj>
2021-02-12 15:57:55 -0600

    Linq sorting

---
+++ PracticeLinq/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PracticeLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vidGames = new List<string> { "Hades", "Spider-Man", "Among Us", "Animal Crossing", "Call of Duty" };
+            vidGames.Sort();
+            Console.WriteLine(string.Join(",", vidGames));
+        }
+    }
+}
